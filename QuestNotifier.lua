8788bce3af188114f89d81111215d93b558c8cc7
Haselnussbomber <mail@haselnussbomber.de>
2016-06-25 19:31:20 +0200

    exclude specific quests by default
    - darkmoon faire items can be sold in auction house. so if they are not excluded, they would notify the user on every login.
    - souting missives are sold from vendor, so they don't need notifications.

--- QuestNotifier.lua
+++ QuestNotifier.lua
@@ -16,7 +16,32 @@ local NUM_BAG_SLOTS = NUM_BAG_SLOTS
 local RaidBossEmoteFrame = RaidBossEmoteFrame
 
 --  serves as exclude list and prevents notifications from repeating
-local questCache = {}
+local questCache = {
+	-- Darkmoon Faire
+	[29443] = true, -- A Curious Crystal
+	[29444] = true, -- An Exotic Egg
+	[29445] = true, -- An Intriguing Grimoire
+	[29446] = true, -- A Wondrous Weapon
+	[29451] = true, -- The Master Strategist
+	[29456] = true, -- A Captured Banner
+	[29457] = true, -- The Enemy's Insignia
+	[29458] = true, -- The Captured Journal
+	[29464] = true, -- Tools of Divination
+
+	-- WoD Garrison: Scouting Missives (Horde / Alliance)
+	[38180] = true, [38193] = true, -- Missive: Assault on the Broken Precipice
+	[38182] = true, [38196] = true, -- Missive: Assault on Darktide Roost
+	[38179] = true, [38192] = true, -- Missive: Assault on the Everbloom Wilds
+	[38178] = true, [38191] = true, -- Missive: Assault on the Iron Siegeworks
+	[38184] = true, [38198] = true, -- Missive: Assault on Lost Veil Anzu
+	[38177] = true, [38190] = true, -- Missive: Assault on Magnarok
+	[38181] = true, [38195] = true, -- Missive: Assault on Mok'gol Watchpost
+	[38185] = true, [38199] = true, -- Missive: Assault on Pillars of Fate
+	[38187] = true, [38201] = true, -- Missive: Assault on Shattrath Harbor
+	[38186] = true, [38200] = true, -- Missive: Assault on Skettis
+	[38183] = true, [38197] = true, -- Missive: Assault on Socrethar's Rise
+	[38176] = true, [38189] = true, -- Missive: Assault on Stonefury Cliffs
+}
 
 function addon:OnEnable()
 	self:RegisterEvent("BAG_UPDATE", "ITEMS_UPDATED")
